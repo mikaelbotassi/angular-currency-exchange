4e0ed9f70594d068063d9ee5e65258775b7a9d05
mikaelbotassi <elkamilive@gmail.com>
2023-10-04 13:55:28 -0300

    style: Creating Global variables in scss

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { HomeComponent } from './pages/home/home.component';
+import { LogoComponent } from './components/icons/logo/logo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    HomeComponent
+    HomeComponent,
+    LogoComponent
   ],
   imports: [
     BrowserModule,

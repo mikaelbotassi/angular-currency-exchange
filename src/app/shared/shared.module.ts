b79f990ac2f682739a9d5b4c7ca8fc177066af05
mikaelbotassi <elkamilive@gmail.com>
2023-10-05 17:00:41 -0300

    Style: Inserting Logo and update colors

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
+import { LogoComponent } from './icons/logo/logo.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [HeaderComponent],
+  declarations: [HeaderComponent, LogoComponent],
   exports: [HeaderComponent]
 })
 export class SharedModule { }

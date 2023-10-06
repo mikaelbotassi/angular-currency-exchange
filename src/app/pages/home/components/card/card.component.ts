e090e59ead6e46fcde53985686881e440325d523
Mikael <elkamilive@gmail.com>
2023-10-05 23:24:29 -0300

    Feat: Request working and completing the card

--- src/app/pages/home/components/card/card.component.ts
+++ src/app/pages/home/components/card/card.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Quote } from '../../models/Quote';
 
 @Component({
   selector: 'app-card',
@@ -6,5 +7,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./card.component.scss']
 })
 export class CardComponent {
+
+  @Input() public quote:Quote = new Quote();
+
+  getFormatedName():string{
+    return this.quote.name.split("/")[0];
+  }
+
+  getformatedValue():string{
+    return Number(this.quote.ask).toLocaleString('pt-BR', {
+      style: 'currency',
+      currency: 'BRL'
+    });
+  }
+  
+  getformatedPercent():string{
+    return this.quote.pctChange.replace(".", ",");
+  }
+  
+  getformatedDate():string{
+    return new Date(this.quote.create_date).toLocaleTimeString('pt-BR', { hour: '2-digit', minute: '2-digit', second: '2-digit' });
+  }
+
+  getTextColor():string{
+    const ask = Number(this.quote.ask);
+    if(ask <= 1) return 'text-red';
+    if(ask <= 5) return 'text-green';
+    return 'text-blue';
+  }
 
 }
--- src/app/pages/home/models/Quote.ts
+++ src/app/pages/home/models/Quote.ts
@@ -1,107 +1,110 @@
 export class Quote{
-    private _codein:string;
-    private _name:string;
-    private _high:string;
-    private _low:string;
-    private _varBid:string;
-    private _pctChange:string;
-    private _bid:string;
-    private _ask:string;
-    private _timestamp:string;
-    private _create_date:string;
+  private _codein:string;
+  private _name:string;
+  private _high:string;
+  private _low:string;
+  private _varBid:string;
+  private _pctChange:string;
+  private _bid:string;
+  private _ask:string;
+  private _timestamp:string;
+  private _create_date:string;
 
-    constructor(){
-        this._codein = "";
-        this._name = "";
-        this._high = "";
-        this._low = "";
-        this._varBid = "";
-        this._pctChange = "";
-        this._bid = "";
-        this._ask = "";
-        this._timestamp = "";
-        this._create_date = "";
-    }
+  constructor(){
+      this._codein = "";
+      this._name = "";
+      this._high = "";
+      this._low = "";
+      this._varBid = "";
+      this._pctChange = "";
+      this._bid = "";
+      this._ask = "";
+      this._timestamp = "";
+      this._create_date = "";
+  }
 
-    get codein(): string {
-        return this._codein;
-      }
-    
-      set codein(value: string) {
-        this._codein = value;
-      }
-    
-      get name(): string {
-        return this._name;
-      }
-    
-      set name(value: string) {
-        this._name = value;
-      }
-    
-      get high(): string {
-        return this._high;
-      }
-    
-      set high(value: string) {
-        this._high = value;
-      }
-    
-      get low(): string {
-        return this._low;
-      }
-    
-      set low(value: string) {
-        this._low = value;
-      }
-    
-      get varBid(): string {
-        return this._varBid;
-      }
-    
-      set varBid(value: string) {
-        this._varBid = value;
-      }
-    
-      get pctChange(): string {
-        return this._pctChange;
-      }
-    
-      set pctChange(value: string) {
-        this._pctChange = value;
-      }
-    
-      get bid(): string {
-        return this._bid;
-      }
-    
-      set bid(value: string) {
-        this._bid = value;
-      }
-    
-      get ask(): string {
-        return this._ask;
-      }
-    
-      set ask(value: string) {
-        this._ask = value;
-      }
-    
-      get timestamp(): string {
-        return this._timestamp;
-      }
-    
-      set timestamp(value: string) {
-        this._timestamp = value;
-      }
-    
-      get create_date(): string {
-        return this._create_date;
-      }
-    
-      set create_date(value: string) {
-        this._create_date = value;
-      }
+  fromName():string{
+    return this._name;
+  }
 
+  get codein(): string {
+    return this._codein;
+  }
+
+  set codein(value: string) {
+    this._codein = value;
+  }
+
+  get name(): string {
+    return this._name;
+  }
+
+  set name(value: string) {
+    this._name = value;
+  }
+
+  get high(): string {
+    return this._high;
+  }
+
+  set high(value: string) {
+    this._high = value;
+  }
+
+  get low(): string {
+    return this._low;
+  }
+
+  set low(value: string) {
+    this._low = value;
+  }
+
+  get varBid(): string {
+    return this._varBid;
+  }
+
+  set varBid(value: string) {
+    this._varBid = value;
+  }
+
+  get pctChange(): string {
+    return this._pctChange;
+  }
+
+  set pctChange(value: string) {
+    this._pctChange = value;
+  }
+
+  get bid(): string {
+    return this._bid;
+  }
+
+  set bid(value: string) {
+    this._bid = value;
+  }
+
+  get ask(): string {
+    return this._ask;
+  }
+
+  set ask(value: string) {
+    this._ask = value;
+  }
+
+  get timestamp(): string {
+    return this._timestamp;
+  }
+
+  set timestamp(value: string) {
+    this._timestamp = value;
+  }
+
+  get create_date(): string {
+    return this._create_date;
+  }
+
+  set create_date(value: string) {
+    this._create_date = value;
+  }
 
 }

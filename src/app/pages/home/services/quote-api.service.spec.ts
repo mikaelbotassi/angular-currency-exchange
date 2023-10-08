290e0a7b5655c642317793aec5709b0e990360c0
Mikael <elkamilive@gmail.com>
2023-10-07 23:24:38 -0300

    Test: Quote service test working

--- src/app/pages/home/services/quote-api.service.spec.ts
+++ src/app/pages/home/services/quote-api.service.spec.ts
@@ -2,18 +2,39 @@ import { TestBed } from '@angular/core/testing';
 
 import { QuoteApiService } from './quote-api.service';
 import { HttpClientModule } from '@angular/common/http';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { Quote } from '../models/Quote';
 
 describe('QuoteApiService', () => {
   let service: QuoteApiService;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [HttpClientModule],
+      imports: [HttpClientModule, HttpClientTestingModule],
+      providers:[QuoteApiService]
     });
+    httpTestingController = TestBed.inject(HttpTestingController)
     service = TestBed.inject(QuoteApiService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should get currency', done => {
+    
+    service.getQuotes("ARS").subscribe({
+      next: (resp) => {
+        expect(testingRequest.request.method).toBe('GET');
+        expect(resp).toEqual([new Quote()]);
+        done();
+      }
+    });
+
+    const testingRequest = httpTestingController.expectOne(req => req.url.includes('ARS'))
+    testingRequest.flush([new Quote()]);
+
+  });
+
 });

cd60c5b13572bb1882876d6368d2d667ccbd2c7d
Mikael <elkamilive@gmail.com>
2023-10-09 21:07:25 -0300

    Doc: New Documentation for Error-Message-Component

---
+++ src/app/pages/home/components/card/error-message/error-message.stories.ts
@@ -0,0 +1,27 @@
+import { componentWrapperDecorator, moduleMetadata, type Meta, type StoryObj } from '@storybook/angular';
+import { ErrorMessageComponent } from './error-message.component';
+
+// More on how to set up stories at: https://storybook.js.org/docs/angular/writing-stories/introduction
+const meta: Meta<ErrorMessageComponent> = {
+  title: 'pages/home/components/card/card-header/ErrorMessageComponent',
+  component: ErrorMessageComponent,
+  decorators:[
+    componentWrapperDecorator((story) =>
+    `
+    <div style="
+    padding:3rem 0;
+    display:flex;
+    align-items:center;
+    justify-content:center
+    ">
+      ${story}
+    </div>
+    `
+  ),
+]
+};
+
+export default meta;
+type Story = StoryObj<ErrorMessageComponent>;
+
+export const Default: Story = {};
